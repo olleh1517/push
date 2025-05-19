bb5d6645eecede9018b4a85542f53f1d9259a5af
yong sang <84034825+youyongsang@users.noreply.github.com>
2025-05-19 15:07:00 +0900

    오류 수정

--- pushotp_server/app.py
+++ pushotp_server/app.py
@@ -17,8 +17,9 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
 
 # Firebase Admin SDK 초기화
 firebase_credentials_path = "/etc/secrets/firebase_credentials.json"
-cred = credentials.Certificate(firebase_credentials_path)
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_credentials_path)
+    firebase_admin.initialize_app(cred)
 
 # 🔄 Firestore 클라이언트는 Firebase 초기화 이후에 호출해야 함
 from firebase_admin import firestore
